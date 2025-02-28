5e59587c66944f59247fa9e33c1c6dd6ec2185a9
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-02-28 12:23:10 +0100

    add state management for movies in HomePage component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,4 +5,16 @@ import Header from './components/Header'
 import Main from './components/Main'
 
 export default function HomePage() {
+    
+        // state to store movies
+        const [movies, setMovies] = useState([])
+    
+        return (
+            <>
+        
+                <Header setMovies={setMovies} />
+                <Main movies={movies} />
+                
+            </>
+        )
 }
