52508c199d05e822054d1baef06f74f190b73d3a
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-02-28 12:52:13 +0100

    import MovieForm in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
+import MovieForm from "./MovieForm"
+
 export default function Header() {
     return (
         <header>
             <h3>BOOLFLIX</h3>
+            <MovieForm />
         </header>
     )
 }
