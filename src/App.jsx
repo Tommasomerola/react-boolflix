326bc834a2a91f4c6faeca97e236835c60ac28b7
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-02-28 12:10:35 +0100

    add movie search functionality and integrate HomePage component

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,29 @@ import {useState} from 'react'
 // import globalContext from './context/GlobalContext'
 import GlobalContext from './context/GlobalContext'
 
+// import HomePage
+import HomePage from './pages/HomePage'
+
 function App() {
 
- 
+ const [movies, setMovies] = useState([])
+
+//  function to search movies
+const searchMovies = (query) => {
+  const APP_KEY = "c0003c38b81b61f4f7861a8395f5c76d"
+  fetch(`https://api.themoviedb.org/3/search/movie?api_key=${APP_KEY}&query=${query}`)
+  .then(res => res.json())
+  // import movies from the response
+  .then(data => setMovies(data.results))
+  .catch(err => console.log(err));
+};
 
   return (
-    <>
-      
-    </>
+
+    <GlobalContext.Provider value={{ movies, searchMovies }}>
+      <Homepage />
+    </GlobalContext.Provider>
+    
   )
 }
 
