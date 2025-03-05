029db8ad41f4a74b85bd38a41cbf1ae16b45e377
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-03-05 10:52:56 +0100

    add tv-series research

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,43 @@ function App() {
  const [movies, setMovies] = useState([])
 
 //  function to search movies
-const searchMovies = (query) => {
+// const searchMovies = (query) => {
+//   const APP_KEY = "c0003c38b81b61f4f7861a8395f5c76d"
+//   fetch(`https://api.themoviedb.org/3/search/movie?api_key=${APP_KEY}&query=${query}`)
+//   .then(res => res.json())
+//   // import movies from the response
+//   .then(data => setMovies(data.results))
+//   .catch(err => console.log(err));
+// };
+
+const searchMoviesAndTv = (query) => {
   const APP_KEY = "c0003c38b81b61f4f7861a8395f5c76d"
-  fetch(`https://api.themoviedb.org/3/search/movie?api_key=${APP_KEY}&query=${query}`)
+
+  // chiamata per i film 
+  const moviePromise = fetch(`https://api.themoviedb.org/3/search/movie?api_key=${APP_KEY}&query=${query}`)
   .then(res => res.json())
-  // import movies from the response
-  .then(data => setMovies(data.results))
+  .then(data => data.results)
+
+  // chiamata per le serie tv
+  const tvPromise = fetch(`https://api.themoviedb.org/3/search/tv?api_key=${APP_KEY}&query=${query}`)
+  .then(res => res.json())
+  .then(data => data.results)
+
+  // aspettiamo che entrambe le chiamate siano completate
+  Promise.all([moviePromise, tvPromise])
+  .then(([moviesData, tvData]) => {
+    const combinedResults = [
+      ...moviesData.map(item => ({ ...item, type: 'movie' })),
+      ...tvData.map(item => ({ ...item, type: 'tv' }))
+    ];
+    setMovies(combinedResults); // Impostiamo i risultati combinati
+  })
   .catch(err => console.log(err));
 };
 
   return (
 
-    <GlobalContext.Provider value={{ movies, searchMovies }}>
+    <GlobalContext.Provider value={{ movies, searchMoviesAndTv }}>
       <HomePage />
     </GlobalContext.Provider>
 
