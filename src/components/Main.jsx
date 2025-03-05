b2376fa833f7bee1fa2aae94da378c15eb6aa2bf
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-03-05 10:31:28 +0100

    add flag recognition

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,25 @@
 import { useContext } from "react";
 import GlobalContext from "../context/GlobalContext";
+import Flag from "react-world-flags";
 
 export default function Main() {
 
     // Accediamo ai film dal contesto
     const { movies } = useContext(GlobalContext);
+
+    // determiniamo la bamdiera adatta
+    const getFlag = (language) => {
+        const flags = {
+            en: 'GB',
+            it: 'IT',
+            es: 'ES',
+            fr: 'FR',
+            de: 'DE',
+        };
+    // se il codice esiste iseriamo la bandiera
+    return flags[language] 
+    }
+
     return (
         <main>
             <h2>Risultati della ricerca</h2>
@@ -14,7 +29,19 @@ export default function Main() {
                         <li key={movie.id}>
                             <h3>{movie.title}</h3>
                             <p><strong>Titolo Originale:</strong> {movie.original_title}</p>
-                            <p><strong>Lingua:</strong> {movie.original_language}</p>
+
+                            <p><strong>Lingua:</strong> {getFlag(movie.original_language) ? (
+                                    <Flag
+                                        code={getFlag(movie.original_language)}
+                                        style={{ width: "30px", height: "20px" }} // Puoi personalizzare le dimensioni
+                                    />
+                                ) : (
+                                   <span>
+                                    Bandiera non trovata, lingua originale: <strong>{movie.original_language}</strong>
+                                   </span> 
+                                )}
+                            </p>
+
                             <p><strong>Voto:</strong> {movie.vote_average}</p>
                         </li>
                     ))}
